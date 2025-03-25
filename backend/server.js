0a3caf1aa8293101046c1dcb4b1f6471e8db0502
ronen <ronenbez@gmail.com>
2025-03-25 11:46:14 +0200

    frontend start

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,15 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/api/products', productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:" + PORT);
 });
 
 // 2wQmTyzDSQI1zWKg
+// youtube min 57
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react';
+
+function App() {
+
+  return (
+    <>
+      <Button>Hello</Button>
+      <h2>test2</h2>
+    </>
+  )
+}
+
+export default App
